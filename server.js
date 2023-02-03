d1a4e6e729120f2ff3a3e0340cfcbdfe5a555091
Andy Zheng <97316979+Andrew-bro2022@users.noreply.github.com>
2023-02-02 23:26:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 /*********************************************************************************
-*  WEB322 – Assignment 5
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
-*  No part of this assignment has been copied manually or electronically from any other source
-*  (including web sites) or distributed to other students.
-* 
-*  Name: __Yuchi Zheng___ Student ID: __025848151__ Date: __2nd, December, 2022__
 *
+*  Author Name: Yuchi Zheng
 *  Online (Cyclic) URL: https://filthy-fez-lamb.cyclic.app/blog
 *
 ********************************************************************************/
